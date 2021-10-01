438d29084b4241fbf6a4987042939d97557182e3
Sudhanshusoni45 <sudhanshusoni45@gmail.com>
2021-10-01 22:15:30 +0530

    update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtAreaInput = document.querySelector("#txtArea-input");
 var outputDiv = document.querySelector("#output");
 
-var serverURL = "https://api.funtranslations.com/translate/groot.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 function getTranslationURL(input) {
   return serverURL + "?" + "text=" + input;
